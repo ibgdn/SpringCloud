34f0a5f1381743d343ef6275b6f48c06523b308c
ibgdn <ibgdn4@gmail.com>
2020-07-04 12:14:25 +0800

    provider（服务注册）基本配置。

---
+++ IBSpringCloud/provider/src/main/java/com/ibgdn/provider/controller/ProviderController.java
@@ -0,0 +1,4 @@
+package com.ibgdn.provider.controller;
+
+public class ProviderController {
+}
---
+++ IBSpringCloud/provider/src/test/java/com/ibgdn/provider/ProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibgdn.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
